98b5355991f83cc9efbfcd07e35862ef5a2a5626
forhad <rhidoy87@yahoo.com>
2020-10-13 21:16:41 +0200

    Add files via upload

---
+++ week9-week9_19.MethodArgumentValidation/src/validation/main.java
@@ -0,0 +1,10 @@
+
+package validation;
+
+public class main {
+    public static void main(String[] args){
+        Person p = new Person("mikae", 0);
+        System.out.println(p);
+        
+    }
+}
